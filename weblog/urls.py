9ce70d87bbd718ce267d874298d9f2baafb61533
hamid <hr19900305@gmail.com>
2022-07-16 13:57:22 +0430

    weblog Def view and details are ok.. templates/urls are ok.

--- weblog/urls.py
+++ weblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import post_view
+from .views import post_view, post_detail
 
 urlpatterns = [
     path('', post_view ),
+    path('/<int:pk>/', post_detail ),
     
     
 ]
--- weblog/views.py
+++ weblog/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.shortcuts import get_object_or_404
+
 
 from models import Posts
 
 
 def post_view(request):
-    post= Posts.objects.all()
-
-    context = {'post': post}
+    posts= Posts.objects.all()
+    context = {'posts': posts}
     return render(request, 'weblog/blog.html', context=context)
+
+
+def post_detail(request,pk):
+    post= get_object_or_404 (Posts, pk=pk)
+    context = {'post': post}
+    return render(request, 'weblog/article.html', context=context)
+    
