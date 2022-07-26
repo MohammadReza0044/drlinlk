6006bc88e9f082bf082133e5b3609000cf7f6a3b
hamid <hr19900305@gmail.com>
2022-07-26 13:08:57 +0430

    api in moshavere: get token and login(get sessionName) is done.

--- moshavere/api.py
+++ moshavere/api.py
@@ -12,15 +12,16 @@ def get_token():
     response = requests.request("GET", url, headers=headers, data=payload)
     token = json.loads(response.text)
     token = token['result']['token']
-    return (str(token))
+    return (str(token + 'vtTd7NZiOKnAMhU'))
 
 
 
 url = "https://drlink.crm24.io/webservice.php"
+login_token = get_token() 
 
 payload={'operation': 'login',
 'username': 'hi@drlink.ir',
-'accessKey':hashlib.md5('vtTd7NZiOKnAMhUget_token()'.encode('utf-8')).hexdigest(),}
+'accessKey':hashlib.md5(login_token.encode('utf-8')).hexdigest(),}
 files=[
 
 ]
