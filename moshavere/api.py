af13baa41abc0519f79075ac46f82d210194dfc4
hamid <hr19900305@gmail.com>
2022-07-26 14:17:08 +0430

    crm api is done and now is connected.

--- moshavere/api.py
+++ moshavere/api.py
@@ -15,19 +15,48 @@ def get_token():
     return (str(token + 'vtTd7NZiOKnAMhU'))
 
 
+def sessionName():
+    url = "https://drlink.crm24.io/webservice.php"
+    login_token = get_token() 
+
+    payload={'operation': 'login',
+    'username': 'hi@drlink.ir',
+    'accessKey':hashlib.md5(login_token.encode('utf-8')).hexdigest(),}
+    files=[
+
+    ]
+    headers = {}
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    session_Name = json.loads(response.text)
+    session_Name = session_Name['result']['sessionName']
+    return(session_Name)
+
+
 
 url = "https://drlink.crm24.io/webservice.php"
-login_token = get_token() 
 
-payload={'operation': 'login',
-'username': 'hi@drlink.ir',
-'accessKey':hashlib.md5(login_token.encode('utf-8')).hexdigest(),}
+payload={'sessionName': sessionName(),
+'operation': 'create',
+'elementType': 'Contacts',
+'element': json.dumps({
+    "assigned_user_id": "19x1",
+    "firstname": "mohammad reza",
+    "lastname": "shaygan",
+    "mobile": "09370946898",
+    "salutationtype": "mr"
+  })
+
+}
+
 files=[
 
 ]
 headers = {}
 
 response = requests.request("POST", url, headers=headers, data=payload, files=files)
-# print(payload)
+
 print(response.text)
+
+
 
