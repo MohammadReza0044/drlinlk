8307d144581a2935926d482f6b8623e3f5a66238
Nima Naraghi <nima.naraghi7@gmail.com>
2022-08-13 18:25:32 +0430

    last blog in home page has been 3 ... search result in search.html has been corected.

--- index/views.py
+++ index/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from weblog.models import Posts
 
 def blog_list(request):
-    posts= Posts.objects.all()[0:6]
+    posts= Posts.objects.all()[0:3]
     return render (request, 'index/index.html', {'posts': posts})
